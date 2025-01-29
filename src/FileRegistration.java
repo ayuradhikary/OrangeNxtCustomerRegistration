bd414aaf90e75c9906dafde9d9b4e89537d16796
ayur.adhikari <ayur.adhikari@fonenxt.com>
2025-01-29 18:00:20 +0545

    minor changes

--- src/FileRegistration.java
+++ src/FileRegistration.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.util.Map;
 
 public class FileRegistration implements Registerable{
-    private Map<String,String> details = new HashMap<String, String>();
+    private Map<String,String> details = new HashMap<>();
     private final String registration_FILE = "registrationdetails.txt";
     File file = new File(registration_FILE);
 
@@ -15,13 +15,23 @@ public class FileRegistration implements Registerable{
         details.put("VerificationCode", verificationCode);
         details.put("MPin", mPin);
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(registration_FILE, true))){
-            String message = "New Customer from " + details.get("country") +
+         /*   String message = "New Customer from %s " + details.get("country") +
                     " with mobile number " + details.get("mobileNumber") +
                     " is registered with verification code " + details.get("VerificationCode") +
-                    " and he/she has set mpin " + details.get("MPin");
-            writer.write(message);
+                    " and he/she has set mpin " + details.get("MPin");*/
+
+           String message = """ 
+            New Customer from %s with mobile number  %s is registered with 
+            verification code  %s and he/she has set mpin %s 
+            """;
+
+            String formattedMessage = message.formatted(details.get("country"),
+                    details.get("mobileNumber"),details.get("VerificationCode"),
+                    details.get("MPin"));
+            writer.write(formattedMessage);
             writer.newLine();
-            System.out.println(message);
+
+            System.out.println(formattedMessage);
         }catch (IOException e){
             System.out.println("could not write to the file");
             e.printStackTrace();
